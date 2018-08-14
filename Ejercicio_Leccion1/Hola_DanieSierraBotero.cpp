545aa51779f8d3aa67b8620d44412f3a14a09ee5
DanielSierra <daniel.sierrab@udea.edu.co>
2018-08-14 14:00:48 -0500

    Ejercicion leccion 1

---
+++ Ejercicio_Leccion1/Hola_DanieSierraBotero.cpp
@@ -0,0 +1,9 @@
+/*
+* Encabezado describiendo el script, los autores y cualquier informacion relevante
+*/
+#include <iostream>		// Para realizar operaciones de entrada y salida
+//using namespace std;
+int main() {			// Inicio de la funcion main
+std::cout << "Hola mundo" << std::endl; 	// Imprime en patalla el texto hola mundo
+return 0;			// Termina la funcion main al retornar un valor
+}				// Final de la funcion main
