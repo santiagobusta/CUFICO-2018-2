e422c464aae870d7e1909dec23379c4d649c38e3
JuanDMV <36173401+JuanDMV@users.noreply.github.com>
2018-08-14 13:47:43 -0500

    Add files via upload
    Ejercicio 1

---
+++ hola Juan Diego.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //Precompiladores - llamados a librerias
+
+using namespace std; //Para realizar operaciones de entrada y salida - sirve para declarar el cout y el end
+
+int main() { //las tabulaciones no son importantes en C++ a diferencia de python 
+	cout << " Hola mundo!" << endl ; //Imprime en pantalla 
+	return 0;
+
+ // Para compilar g++ Hola Hola.cpp, para ejecutar ./Hola 
+}
