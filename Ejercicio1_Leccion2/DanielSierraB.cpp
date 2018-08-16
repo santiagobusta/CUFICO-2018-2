8464c61d7b7e71c1c3d6889c161a6331b66d82d0
DanielSierra <daniel.sierrab@udea.edu.co>
2018-08-16 13:36:07 -0500

    Holi

---
+++ Ejercicio1_Leccion2/DanielSierraB.cpp
@@ -0,0 +1,14 @@
+/*
+*Ciclo While
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+double b = 1.001;
+while (b<10000){
+b*=1.001;
+cout << b << endl;
+}
+return 0;
+}
---
+++ Ejercicio1_Leccion2/DanielSierraB.py
@@ -0,0 +1,6 @@
+# Ciclo while
+
+b = 1.001
+while (b<1000):
+	b*=1.001
+	print(b)
