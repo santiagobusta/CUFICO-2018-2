09a1d8cc113e183506c677dcc6c8c1c17399d9a3
MarianaJaramillo <36014527+MarianaJaramillo@users.noreply.github.com>
2018-08-16 21:44:22 -0500

    Add files via upload

---
+++ Ejercicio1_Leccion2/Mariana_Jaramillo.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+int main(){
+
+float b = 1.001;
+while (b<10000){
+b*=1.001;
+
+cout << b << endl;
+}
+    return 0;
+
+
+}
+
+/*real	0m0,031s
+user	0m0,012s
+sys	0m0,019s */
