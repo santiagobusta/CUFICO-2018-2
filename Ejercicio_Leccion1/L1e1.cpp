4b6e0ebf6fd1a3c76de1ad431f1021cb83bbd541
Diego10HDZ <42388481+Diego10HDZ@users.noreply.github.com>
2019-02-20 18:00:51 -0500

    Add files via upload

---
+++ Ejercicio_Leccion1/L1e1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// Para realizar operaciones de entrada y salida
+using namespace std; // si quitamos esto  se generará un error al compilar
+int main() {
+
+// Inicio de la funcion main
+cout << "Hola mundo" << endl; // Imprime en patalla el texto hola mundo
+return 0;
+}
+// Termina la funcion main al retornar un valor
+// Final de la funcion main
---
+++ Ejercicio_Leccion1/L1e2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// Para realizar operaciones de entrada y salida
+//using namespace std; // solucionaremos este problema sin usar namespace
+int main() {
+
+// Inicio de la funcion main
+std::cout << "Hola mundo" ;
+std::cout << std::endl;
+return 0;
+}
+// Termina la funcion main al retornar un valor
+// Final de la funcion main
